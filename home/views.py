5721d95880f5ddfc12386a209fb323466350ea12
agatamalucha <agata.malucha@gmail.com>
2021-07-04 16:04:56 +0100

    upload message

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def home(request):
+    if request.method == "POST":
+        new_message = "Successful upload!"
+        return render(request, "home.html", {"new_message": new_message},)
     return render(request, "home.html", )
 
