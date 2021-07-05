485b6e9cc009f5ed22bde5b23ab56362f44f5c63
agatamalucha <agata.malucha@gmail.com>
2021-07-05 17:44:14 +0100

    new message pd

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from home.views import home
+from home.views import home, dashboard
 
 app_name = "home"
 
 urlpatterns = [
     path("", home, name="home"),
+    path("dashboard",dashboard, name="dashboard")
 ]
--- home/views.py
+++ home/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from django.contrib import messages
+import pandas as pd
+
 
 
 def home(request):
     if request.method == "POST":
-        new_message = "Successful upload!"
+        csv_file=request.FILES["csv_file"]
+        dataset = pd.read_csv(csv_file, encoding="utf-8-sig")
+        new_message = str(dataset.columns)
         return render(request, "home.html", {"new_message": new_message},)
     return render(request, "home.html", )
 
+def dashboard(request):
+    return render(request, "dashboard.html", )
